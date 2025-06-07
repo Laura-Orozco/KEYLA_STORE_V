dff87303ad000e13b4db7df90c378634d9274200
Laura-Orozco <lauraorozco1223@gmail.com>
2025-06-06 22:31:18 -0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://root:SQanlBxvgLoVyAxZaInoBOrEkzfpZPBr@ballast.proxy.rlwy.net:14976/railway"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:SQanlBxvgLoVyAxZaInoBOrEkzfpZPBr@ballast.proxy.rlwy.net:14976/railway"
 
 
 engine = create_engine(
